1d5fa946809c9235fe71a25e831e71663f77bc5f
temagr <postagric@gmail.com>
2019-09-21 04:46:19 +0300

    Remove unused files

--- projects/ngx-select-flags/src/public-api.ts
+++ projects/ngx-select-flags/src/public-api.ts
@@ -1,7 +1,2 @@
-/*
- * Public API Surface of ngx-select-flags
- */
-
-export * from './lib/ngx-select-flags.service';
 export * from './lib/ngx-select-flags.component';
 export * from './lib/ngx-select-flags.module';
