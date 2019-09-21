e0957d1611d126876ae3aaf70d1211020afee394
temagr <postagric@gmail.com>
2019-09-21 05:10:20 +0300

    Include NgxSelectFlags lib to example app

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgxSelectFlagsModule } from 'ngx-select-flags';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSelectFlagsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
