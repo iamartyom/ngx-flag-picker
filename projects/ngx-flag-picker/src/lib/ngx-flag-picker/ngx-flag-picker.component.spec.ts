c655eee88dcac5682d26356c60ba6a338234d85b
Artyom Gritsuk <postagric@gmail.com>
2020-12-03 11:34:49 +0300

    Migrate Angular 10 to Angular 11

--- projects/ngx-flag-picker/src/lib/ngx-flag-picker/ngx-flag-picker.component.spec.ts
+++ projects/ngx-flag-picker/src/lib/ngx-flag-picker/ngx-flag-picker.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NgxFlagPickerComponent } from './ngx-flag-picker.component';
 
 describe('NgxFlagPickerComponent', () => {
   let component: NgxFlagPickerComponent;
   let fixture: ComponentFixture<NgxFlagPickerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgxFlagPickerComponent ]
     })
